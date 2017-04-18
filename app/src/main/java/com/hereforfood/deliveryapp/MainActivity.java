86ec0478712df560f8dc63250eee56e70beaa26c
Shreyak Kumar <kumarshreyak@gmail.com>
2017-04-18 15:56:08 +0530

    Added Maps Activity

--- app/src/main/java/com/hereforfood/deliveryapp/MainActivity.java
+++ app/src/main/java/com/hereforfood/deliveryapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hereforfood.deliveryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b;
+        Intent intent = new Intent(this, MapsActivity.class);
+        //intent.setClassName(this, "MapsActivity.java");
+        startActivity(intent);
     }
 }
