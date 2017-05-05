534651e089138ee75fad2cbd435cc2e6cf59652b
Shreyak Kumar <kumarshreyak@gmail.com>
2017-05-05 19:34:02 +0530

    Tested User node with fake data

--- app/src/main/java/com/hereforfood/deliveryapp/Locality.java
+++ app/src/main/java/com/hereforfood/deliveryapp/Locality.java
@@ -1,6 +1,7 @@
 package com.hereforfood.deliveryapp;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,14 +10,17 @@ import java.util.List;
 
 public class Locality {
 
-    private int id;
-    private String AddressLine1, city, landmark;
-    private double latitude;
-    private double longitude;
-    private int isCompleted = 0, housesLeft;
-    private Date date;
-    private List<House> house ;
+    public int id;
+    String AddressLine1, city, landmark;
+    double latitude;
+    double longitude;
+    int isCompleted = 0, housesLeft;
+    Date date;
+    List<House> house ;
 
-    public Locality() { ; }
+
+    public Locality() {
+        house = new ArrayList<House>();
+    }
 
 }
--- app/src/main/java/com/hereforfood/deliveryapp/User.java
+++ app/src/main/java/com/hereforfood/deliveryapp/User.java
@@ -1,5 +1,6 @@
 package com.hereforfood.deliveryapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +22,44 @@ public class User {
     public User(String userId, String userEmail) {
         this.userId = userId;
         this.userEmail = userEmail;
+        locality = new ArrayList<Locality>();
+
         // TODO insert dummy values in locality list and insert it into firebase
+        Locality l = new Locality();
+
+        // Adding locality with id = 21
+        l.id = 21;
+        l.AddressLine1 = "lorem ipsum";
+        l.city = " MUMBAI";
+        List<House> houses = new ArrayList<House>();
+        House h = new House();
+        h.id = 1;
+        l.house.add(h);
+        h.id = 2;
+        l.house.add(h);
+        h.id = 3;
+        l.house.add(h);
+        h.id = 4;
+        l.house.add(h);
+        h.id = 56;
+        l.house.add(h);
+        locality.add(l);
+
+        // Adding locality with id = 69
+        l.id = 69;
+        l.AddressLine1 = "loremsdada ipsum";
+        l.city = "NOIDA";
+        h.id = 10;
+        l.house.add(h);
+        h.id = 20;
+        l.house.add(h);
+        h.id = 30;
+        l.house.add(h);
+        h.id = 40;
+        l.house.add(h);
+        h.id = 50;
+        l.house.add(h);
+        locality.add(l);
 
     }
 }
