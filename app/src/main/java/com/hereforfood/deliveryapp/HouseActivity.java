4d3ecd0401af3ad594fcae1fe43db0bc79c5c473
Shreyak Kumar <kumarshreyak@gmail.com>
2017-07-16 21:57:49 +0530

    Setup the Deliver activity

--- app/src/main/java/com/hereforfood/deliveryapp/HouseActivity.java
+++ app/src/main/java/com/hereforfood/deliveryapp/HouseActivity.java
@@ -1,11 +1,13 @@
 package com.hereforfood.deliveryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HouseActivity extends AppCompatActivity {
@@ -22,12 +24,23 @@ public class HouseActivity extends AppCompatActivity {
         // TODO Add elements to this activity and make a "Deliver" button (and its activity)
         TextView houseAddrText = (TextView) findViewById(R.id.AddrText);
         TextView delStatText = (TextView) findViewById(R.id.DeliText);
+
+        // Setting the house's address
         houseAddrText.setText(house.getAddress());
+
+        // Displaying status of the delivery
         if(house.isComplete())
             delStatText.setText("Delivery :- Completed");
         else
             delStatText.setText("Delivery :- Incomplete");
+    }
 
+    // Deliver button's onclick method
+    public void delButtonClick(View v)
+    {
+        Intent intent = new Intent(this, DeliveryActivity.class);
+        intent.putExtra("house", house);
+        startActivity(intent);
     }
 
 }
