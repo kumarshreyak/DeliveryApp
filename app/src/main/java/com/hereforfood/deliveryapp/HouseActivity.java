efc6ce85ac50f5964b842ac3938b9c5c936cff74
Shreyak Kumar <kumarshreyak@gmail.com>
2017-07-17 18:11:07 +0530

    Implemented a better dialog (Alert Dialog)

--- app/src/main/java/com/hereforfood/deliveryapp/HouseActivity.java
+++ app/src/main/java/com/hereforfood/deliveryapp/HouseActivity.java
@@ -1,10 +1,14 @@
 package com.hereforfood.deliveryapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -38,8 +42,26 @@ public class HouseActivity extends AppCompatActivity {
     // Deliver button's onclick method
     public void delButtonClick(View v)
     {
-        // TODO Open callingActivity pop window and check if the update to the DB updates this page too
-        DeliveryConfirmDialog dialog = new DeliveryConfirmDialog(HouseActivity.this);
+        // Built a Alert dialog for confirming delivery
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.DeliveryConfirmDialog));
+
+        // Adding the text
+        builder.setMessage("For report id ....").setTitle("Confirm Delivery ?");
+
+        // Adding the buttons
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked YES button
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked NO Button
+            }
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
         dialog.show();
     }
 
